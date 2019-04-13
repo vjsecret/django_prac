c053025ea433921361f5eddd7f2ab70e551697b2
fred <fred@askey.com.tw>
2019-04-13 12:17:44 +0800

    Revert "prac1_ver1"
    This reverts commit 61866ac2d51799ecdb751d7f7dee4f9e96454ca7.

--- ithome/func1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from func1.models import Music
-
-admin.site.register(Music)
--- ithome/func1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Func1Config(AppConfig):
-    name = 'func1'

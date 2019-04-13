61866ac2d51799ecdb751d7f7dee4f9e96454ca7
fred <fred@askey.com.tw>
2019-04-13 12:07:15 +0800

    prac1_ver1

---
+++ ithome/func1/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+
+def index(request):
+    return render(request,'index.html')
+
+def member(request):
+    return render(request,'member.html')
+    
+def mycrud(request):
+    return render(request,'mycrud.html')
+    
+def login(request):
+    return render(request,'login.html')
+     
+def car(request):
+    return render(request,'car.html')
+    
+def testindex(request):
+    #return HttpResponse('歡迎光臨我的網站')
+    return render(request,'testindex.html')
+
+# Create your views here.
+def listone(request):
+    print("hello web")
+    return render(request,'basic.html',locals())
+    #return HttpResponse("hello world")
+
+# Create your views here.
+def hello_view(request):
+    return render(request, 'hello_django.html', {
+        'data': "Hello Django ",
+    })
+
+
+def face(request, kind):
+    return render(request,'face.html', {'kind':kind})
+
+def printPage(request):
+    # with open('templates/print.html','r',encoding="utf-8") as reader:
+    #         t = template.Template(reader.read())
+    #         c = template.Context({ 'name':'s1', 'price':60, 'comment':'deli', 'is_spicy':False })
+    #         return HttpResponse(t.render(c))
+
+    food1 = { 'name':'番茄炒蛋', 'price':60, 'comment':'好吃', 'is_spicy':False }
+    food2 = { 'name':'蒜泥白肉', 'price':100, 'comment':'人氣推薦', 'is_spicy':True }
+    foods = [food1,food2]
+    # return render_to_response('print.html',locals())
+    return render(request,'print.html',locals())
+
+def cama(request):
+    image = cv2.imread('..\static\images\Desert.jpg',1)
+    # cv2.imshow("demo", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # cap = cv2.VideoCapture(0)
+    # while(True):
+    #     ret , frame = cap.read()
+    #     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #     cv2.imshow('frame',gray)
+    #     if cv2.waitKey(1) & 0xFF ==ord('q'):
+    #     break
+    # cap.release()
+    # cv2.destroyAllWindows()
---
+++ ithome/ithome/urls.py
@@ -0,0 +1,40 @@
+"""ithome URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from func1 import views
+# from func1.views import hello_view
+# from func1.views import listone
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.testindex),
+    path('index', views.index),
+    path('member', views.member),
+    path('mycrud', views.mycrud),
+    path('login', views.login),
+    path('car', views.car),
+    path('listone', views.listone),
+    path('hello_view', views.hello_view),
+    path('print', views.printPage),
+    path('face/<int:kind>/', views.face),
+    path('cama', views.cama),
+
+    # path(r'^hello/', hello_view),
+    #path('face', views.face),
+    #re_path('face/(\d{1,2})/', views.face),
+    #path(r'^face/(?P<kind>[0-9]', views.face),
+]

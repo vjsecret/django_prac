08452e55d78f9bfa5ace1f2a3f9498e3abb985a8
fred <fred@askey.com.tw>
2019-06-20 14:43:06 +0800

    use mysql and imort admin

--- ithome/func1/models.py
+++ ithome/func1/models.py
@@ -8,6 +8,7 @@ from django.db import models
 #     """ track of changes """
 #     version = models.IntegerField(default=0)
 
+#http://dokelung-blog.logdown.com/posts/220606-django-notes-5-model-and-database
 # Create your models here.
 class Music(models.Model):
     song = models.TextField(default="song")
@@ -17,3 +18,40 @@ class Music(models.Model):
 
     class Meta:
         db_table = "music"
+        
+class Profile(models.Model):
+    name     = models.CharField(max_length = 50)
+    age      = models.IntegerField()
+    tel      = models.CharField(max_length = 30)
+    address  = models.CharField(max_length = 100)
+    email    = models.EmailField()
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta(object):
+        db_table = "profile"
+
+
+class User(models.Model):
+    username = models.CharField(max_length = 30)
+    def __unicode__(self):
+        return self.username
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=50, blank=True)
+
+    class Meta(object):
+        db_table = "restaurant"
+    
+class Food(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=3,decimal_places=0)
+    comment = models.CharField(max_length=50, blank=True)
+    is_spicy = models.BooleanField()
+    #restaurant = models.ForeignKey(Restaurant)
+
+    class Meta(object):
+        db_table = "food"
--- ithome/func1/views.py
+++ ithome/func1/views.py
@@ -1,21 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import template
+from django.contrib import auth
+import pymysql
+import datetime
+
+def selecAttr(str):
+    Now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #str(Now)
+    if str== "comic":
+        db = pymysql.connect(host="localhost", user="test1", password="123", db="comic")
+        cursor = db.cursor()
+        # 按字典返回 
+        # cursor = db.cursor(pymysql.cursors.DictCursor)
+        getlist("comic")
+        db.close()
+        food1 = { 'id':1,'title':'漫畫一', 'photoid':3, 'content':'漫畫一好吃'}
+        food2 = { 'id':2,'title':'漫畫一蒜泥白肉', 'photoid':4, 'content':'漫畫一人氣推薦'}
+        return [food1,food2]
+    if str== "article":
+        food1 = { 'id':1,'title':'文章一', 'photoid':3, 'content':'文章一好吃'}
+        food2 = { 'id':2,'title':'文章一蒜泥白肉', 'photoid':4, 'content':'文章一人氣推薦'}
+        return [food1,food2]    
 
 def index(request):
     return render(request,'index.html')
 
-def member(request):
-    return render(request,'member.html')
-    
+# def member(request):
+#     return render(request,'member.html')
+def member(request,attr):
+    if attr== "info":
+        return render(request,'member.html')
+    if attr== "manager":
+        return render(request,'manager.html')
+def manager(request,attr):
+    if attr== "info":
+        return render(request,'member.html')
+    else:
+    #if attr== "manager":
+        return render(request,'manager.html')
+
 def mycrud(request):
     return render(request,'mycrud.html')
-# def login(request):
-#    return render(request,'login.html')
+def login(request):
+   return render(request,'login.html')
 def car(request):
     return render(request,'car.html')
     
 def testindex(request):
+    print(type(request.user))
+    #print(request.user.is_authenticated())
     #return HttpResponse('歡迎光臨我的網站')
     return render(request,'testindex.html')
 
@@ -33,6 +67,7 @@ def hello_view(request):
 
 
 def face(request, kind):
+    print(type(kind))
     return render(request,'face.html', {'kind':kind})
 
 def printPage(request):
@@ -63,7 +98,142 @@ def cama(request):
     # cap.release()
     # cv2.destroyAllWindows()
 def pa_comic(request):
+    db = pymysql.connect(host="localhost", user="test1", password="123", db="comic")
+
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS clist")
+
+    sql = """CREATE TABLE `clist` (
+    `id` int(10) DEFAULT NULL,
+    `title` char(20) NOT NULL,
+    `content` char(20) DEFAULT NULL,
+    `photoid` int(11) DEFAULT NULL
+    )"""
+    cursor.execute(sql)
+    print("Created table Successfull.")
+
+    sql = """INSERT INTO clist(ID, TITLE,
+    CONTENT, PHOTOID)
+    VALUES (1,'漫畫一', '漫畫一好吃', 3)"""
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    
+    # 再次插入一条记录
+    sql = """INSERT INTO CLIST(ID, TITLE,
+    CONTENT, PHOTOID)
+    VALUES (2,'漫畫一蒜泥白肉', '漫畫一人氣推薦', 4)"""
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    #print (sql)
+    print('Yes, Insert Successfull.')
+    db.close()
     return render(request,'pa_comic.html')
+def pa_article(request):
+    db = pymysql.connect(host="localhost", user="test1", password="123", db="test")
+
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS employee")
+    # Create table as per requirement
+    sql = """CREATE TABLE `employee` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `first_name` char(20) NOT NULL,
+    `last_name` char(20) DEFAULT NULL,
+    `age` int(11) DEFAULT NULL,
+    `sex` char(1) DEFAULT NULL,
+    `income` float DEFAULT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"""
+    cursor.execute(sql)
+    print("Created table Successfull.")
+    sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+    LAST_NAME, AGE, SEX, INCOME)
+    VALUES ('Mac', 'Su', 20, 'M', 5000)"""
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+    
+    # 再次插入一条记录
+    # Prepare SQL query to INSERT a record into the database.
+    sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+    LAST_NAME, AGE, SEX, INCOME)
+    VALUES ('Kobe', 'Bryant', 40, 'M', 8000)"""
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Commit your changes in the database
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+    #print (sql)
+    print('Yes, Insert Successfull.')
+    db.close()
+    return render(request,'pa_article.html')
+def getlist(str):
+    if str== "comic":
+        # Prepare SQL query to select a record from the table.
+        sql = "SELECT * FROM CLIST"
+        #\
+        #    WHERE INCOME > %d" % (1000)
+        #print (sql)
+        try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Fetch all the rows in a list of lists.
+            results = cursor.fetchall()
+            for row in results:
+                #print (row)
+                id = row[0]
+                title = row[1]
+                content = row[2]
+                photoid = row[3]
+                # Now print fetched result
+                print ("id = %s, title = %s, content = %s, photoid = %s" % \
+                        (id, title, content, photoid ))
+        except:
+            import traceback
+            traceback.print_exc()
+            print ("Error: unable to fetch data")
+
+def comic(request):
+    list = selecAttr("comic")
+    # food1 = { 'id':1,'title':'漫畫一', 'photoid':3, 'content':'好吃'}
+    # food2 = { 'id':2,'title':'蒜泥白肉', 'photoid':4, 'content':'人氣推薦'}
+    # comiclist = [food1,food2]
+    return render(request,'comic.html',locals())
+
+def comic(request, data):
+    print(data)
+    print(type(data))
+    #list = selecAttr(attr)
+    # food1 = { 'id':1,'title':'漫畫一', 'photoid':3, 'content':'好吃'}
+    # food2 = { 'id':2,'title':'蒜泥白肉', 'photoid':4, 'content':'人氣推薦'}
+    # comiclist = [food1,food2]
+    # return redirect
+    return render(request,'comic.html',{'abc': "Hello Django "},{locals() } )
+
+def article(request):
+    list = selecAttr("article")
+    return render(request,'article.html',locals())
+
+def testurl(request, attr):
+    #article(request)
+    #return redirect
+    #print(type(attr))
+    #print(type(abc)
+    list = selecAttr(attr)
+    return render(request,'testurl.html',locals())
 # def welcome(request):
 #     if 'user_name' in request.GET:
 #         return HttpResponse('Welcome!~'+request.GET['user_name'])
